52bc9ea00aa2e55af18d7b157d97fa4ed9e29e74
Ziyan Wang <wangziyannb@gmail.com>
2022-04-15 04:04:07 -0400

    init

---
+++ samples/nucleus/part2.py
@@ -0,0 +1,10 @@
+# #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/15 3:46
+# @Author  : Wang Ziyan
+# @Email   : 1269586767@qq.com
+# @File    : part2.py
+# @Software: PyCharm
+
+if __name__ == '__main__':
+    print("")
